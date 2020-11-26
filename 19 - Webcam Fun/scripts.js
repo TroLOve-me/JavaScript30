1d24e5753aa26aa2ef4fe6065964410745d8636f
zjh <18020262879@163.com>
2020-11-26 08:50:25 +0800

    get a few ideas about my camera

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,62 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            //video.src = window.URL.createObjectURL(localMediaStream);
+            video.srcObject = localMediaStream;
+            video.play();
+        })
+        .catch(error => console.log(`oh no`, error));
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height)
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.8;
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+//mass with pixels
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100; //r
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //r
+        pixels.data[i + 100] = pixels.data[i + 1]; //g
+        pixels.data[i - 150] = pixels.data[i + 2]; //b
+    }
+    return pixels;
+}
+
+function takePhoto() {
+    snap.currentTime = 0;
+    snap.play();
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'pretty');
+    link.textContent = "DownLoad Image";
+    strip.insertBefore(link, strip.firstChild);
+}
+getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas)
